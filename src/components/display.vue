2f39f4ab44d22ed53e8b1ffd786f991d51248ade
Meshileya Seun <meshileyaseun@gmail.com>
2018-09-18 17:01:27 +0100

    update file with getters sample

--- src/components/display.vue
+++ src/components/display.vue
@@ -6,37 +6,41 @@
 </template>
 
 <script>
-    import {
-        mapGetters
-    } from 'vuex';
-    
-    export default {
-        name: 'appResult',
-    
-        /**
-         * map the method(s) in getters to a property
-         */
-        // computed: mapGetters({
-        //     counter: 'addCurrencyToCounter',
-        //     increment: 'incrementCounterByTen'
-        // })
-    
-        /**
-         * using spread operator
-         * This is useful when other properties are to be 
-         * added to computed proptery
-         */
-        computed: {
-            ...mapGetters({
-                counter: 'addCurrencyToCounter',
-                increment: 'incrementCounterByTen'
-            })
-        }
+import { mapGetters } from "vuex";
+
+export default {
+  name: "appResult",
+
+//   computed: {
+//     counter() {
+//       return this.$store.getters.addCurrencyToCounter;
+//     }
+//   }
+
+  /**
+   * map the method(s) in getters to a property
+   */
+  // computed: mapGetters({
+  //     counter: 'addCurrencyToCounter',
+  //     increment: 'incrementCounterByTen'
+  // })
+
+  /**
+   * using spread operator
+   * This is useful when other properties are to be
+   * added to computed proptery
+   */
+    computed: {
+      ...mapGetters({
+        counter: "addCurrencyToCounter",
+        increment: "incrementCounterByTen"
+      })
     }
+};
 </script>
 
 <style>
-    p {
-        font-size: 40px;
-    }
+p {
+  font-size: 40px;
+}
 </style>
